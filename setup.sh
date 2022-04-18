175d1f384a1e45b9fe51adc04c34b9245146e9ba
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-04-18 10:56:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+if [ "$(id -u)" == "0" ]; then
+  sleep 0.1
+  echo -e "[*] Creating the setup for working properly."
+  mkdir /opt/WEF 2>/dev/null
+  mkdir /opt/WEF/main 2>/dev/null
+  mdkir /opt/WEF/main/img 2>/dev/null
+  mkdir /opt/WEF/main/modules 2>/dev/null
+  mkdir /opt/WEF/main/wordlists 2>/dev/null
+  mdkir /opt/WEF/main/captures 2>/dev/null
 
-echo -e ""
-mkdir /opt/WEF
-mkdir /opt/WEF/main
-mdkir /opt/WEF/main/img
-mkdir /opt/WEF/main/modules
-mkdir /opt/WEF/main/wordlists
-mdkir /opt/WEF/main/captures
-
-wget "https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz" -o /opt/WEF/main/wordlists/rockyou.txt.gz
-gunzip /opt/WEF/main/wordlists/rockyou.txt.gz
-wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt" -o /opt/WEF/main/wordlists/wordlists/probable-v2-wpa-top4800.txt
-wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top10000.txt" -o /opt/WEF/main/wordlists/darkweb2017-top10000.txt
-# wget "kaonashi" -o /opt/WEF/main/wordlists/
-wget "https://raw.githubusercontent.com/LionSec/xerosploit/master/tools/bettercap/modules/replace_images.rb" -o /opt/WEF/main/modules/replace_images.rb
-wget "WEF.sh" -o /opt/WEF/WEF
-chmod +x /opt/WEF/WEF
-wget "WEF.sh" -o /usr/bin/WEF &>/dev/null
-chmod +x /usr/bin/WEF
+  wget "https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz" -o /opt/WEF/main/wordlists/rockyou.txt.gz &>/dev/null
+  gunzip /opt/WEF/main/wordlists/rockyou.txt.gz 2>/dev/null
+  wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt" -o /opt/WEF/main/wordlists/probable-v2-wpa-top4800.txt &>/dev/null
+  wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top10000.txt" -o /opt/WEF/main/wordlists/darkweb2017-top10000.txt &>/dev/null
+  # wget "kaonashi" -o /opt/WEF/main/wordlists/
+  wget "https://raw.githubusercontent.com/LionSec/xerosploit/master/tools/bettercap/modules/replace_images.rb" -o /opt/WEF/main/modules/replace_images.rb &>/dev/null
+  wget "https://raw.githubusercontent.com/D3Ext/WEF/main/WEF.sh" -o /opt/WEF/WEF &>/dev/null
+  chmod +x /opt/WEF/WEF 2>/dev/null
+  cp /opt/WEF/WEF /usr/bin/WEF &>/dev/null
+  chmod +x /usr/bin/WEF 2>/dev/null
+else
+  echo -e "[X] Please, execute the script as root"
+  exit 1
+fi
