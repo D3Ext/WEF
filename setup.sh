565a4a56f9cb27c720f31474856238f14c3d166a
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-05-07 18:45:02 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,32 +3,33 @@
 if [ "$(id -u)" == "0" ]; then
 sleep 0.1
 	adir=$(pwd)
-	echo -e "\n[WEF] Creating the setup for working properly.\n"
-	mkdir /opt/WEF 2>/dev/null
-	mkdir /opt/WEF/main 2>/dev/null
-	mkdir /opt/WEF/main/img 2>/dev/null
-	mkdir /opt/WEF/main/modules 2>/dev/null
-	mkdir /opt/WEF/main/wordlists 2>/dev/null
-	mkdir /opt/WEF/main/captures 2>/dev/null
-	mkdir /opt/WEF/main/logs 2>/dev/null
+	echo -e "\n[WEF] Creating the setup for working properly."
+	mkdir /opt/wef 2>/dev/null
+	mkdir /opt/wef/main 2>/dev/null
+	mkdir /opt/wef/main/img 2>/dev/null
+	mkdir /opt/wef/main/modules 2>/dev/null
+	mkdir /opt/wef/main/wordlists 2>/dev/null
+	mkdir /opt/wef/main/captures 2>/dev/null
+	mkdir /opt/wef/main/logs 2>/dev/null
+	mv templates /opt/wef/main/ 2>/dev/null 
 	wget "https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz" &>/dev/null
-	mv rockyou.txt.gz /opt/WEF/main/wordlists/rockyou.txt.gz 2>/dev/null
-	gunzip /opt/WEF/main/wordlists/rockyou.txt.gz 2>/dev/null
+	mv rockyou.txt.gz /opt/wef/main/wordlists/rockyou.txt.gz 2>/dev/null
+	gunzip /opt/wef/main/wordlists/rockyou.txt.gz 2>/dev/null
 	wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt" &>/dev/null
-	mv probable-v2-wpa-top4800.txt /opt/WEF/main/wordlists/ 2>/dev/null
+	mv probable-v2-wpa-top4800.txt /opt/wef/main/wordlists/ 2>/dev/null
 	wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top10000.txt" &>/dev/null
-	mv darkweb2017-top10000.txt /opt/WEF/main/wordlists/ 2>/dev/null
+	mv darkweb2017-top10000.txt /opt/wef/main/wordlists/ 2>/dev/null
 	wget "https://raw.githubusercontent.com/LionSec/xerosploit/master/tools/bettercap/modules/replace_images.rb" &>/dev/null
-	mv replace_images.rb /opt/WEF/main/modules/ 2>/dev/null
-	cp ${adir}/WEF /usr/bin/wef 2>/dev/null
-	cp ${adir}/WEF /opt/WEF/WEF 2>/dev/null
-	cp ${adir}/clear.sh /opt/WEF/clear-logs.sh 2>/dev/null
-	chmod +x WEF 2>/dev/null
-	chmod +x /opt/WEF/WEF 2>/dev/null
+	mv replace_images.rb /opt/wef/main/modules/ 2>/dev/null
+	cp ${adir}/wef /usr/bin/wef 2>/dev/null
+	cp ${adir}/wef /opt/wef/wef 2>/dev/null
+	cp ${adir}/clear.sh /opt/wef/clear-logs.sh 2>/dev/null
+	chmod +x wef 2>/dev/null
+	chmod +x /opt/wef/wef 2>/dev/null
 	chmod +x /usr/bin/wef 2>/dev/null
-	chmod +x /opt/WEF/clear-logs.sh 2>/dev/null
+	chmod +x /opt/wef/clear-logs.sh 2>/dev/null
 	rm ${adir}/setup.sh 2>/dev/null
-	echo -e "[+] Installation completed"
+	echo -e "\n[+] Installation completed"
 else
 	echo -e "\n[X] Please, execute the script as root.\n"
 	exit 1
