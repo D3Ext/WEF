029060e4ba9ebde57eb175d0473d277c469dff35
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-06-08 14:49:34 +0000

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo -ne "\n${blueColour}[${endColour}${yellowColour}+${endColour}${blueColour}] Uninstalling WEF"; sleep 0.5; echo -ne ""; sleep 0.5; echo -ne ""; sleep 0.5; echo -ne ".${endColour}"; sleep 0.5
+yellowColour="\e[0;33m\033[1m"
+blueColour="\e[0;34m\033[1m"
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+
+echo -ne "\n${blueColour}[${endColour}${yellowColour}+${endColour}${blueColour}] Uninstalling WEF"; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne ".${endColour}"; sleep 0.5
 rm -rf /opt/wef 2>/dev/null
 rm /usr/bin/wef 2>/dev/null
 sleep 1
-echo -e "\n${blueColour}[${endColour}${yellowColour}+${endColour}${blueColour}] Uninstalled successfully${endColour}"
+echo -e "\n${blueColour}[${endColour}${greenColour}+${endColour}${blueColour}] WEF uninstalled successfully${endColour}"
 sleep 0.3
