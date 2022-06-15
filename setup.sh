c5e4a6d0276d105a5abbeb72165b3376f44e7194
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-06-15 18:30:17 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,22 @@ sleep 0.1
 	adir=$(pwd)
 	echo -e "\n[WEF] Preparing the setup for working properly."
 	sleep 0.5
-	echo -ne "\n[+] Creating directories."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2
+	echo -ne "\n[+] Creating directories"; sleep 0.3; echo -ne "."; sleep 0.3; echo -ne "."; sleep 0.3; echo -ne "."; sleep 0.3
 	mkdir /opt/wef 2>/dev/null
 	mkdir /opt/wef/main 2>/dev/null
-	mkdir /opt/wef/main/img 2>/dev/null
 	mkdir /opt/wef/main/bluetooth 2>/dev/null
 	mkdir /opt/wef/main/wordlists 2>/dev/null
 	mkdir /opt/wef/main/captures 2>/dev/null
 	mkdir /opt/wef/main/logs 2>/dev/null
+	mkdir /opt/wef/extra 2>/dev/null
+	touch /opt/wef/extra/delete-creds.sh
+	chmod +x /opt/wef/extra/delete-creds.sh
+	echo '#!/bin/bash' > /opt/wef/extra/delete-creds.sh
+	echo 'echo "" > /opt/wef/main/templates/*/datos-privados.txt' >> /opt/wef/extra/delete-creds.sh
+	echo 'echo "" > /opt/wef/main/templates/*/usernames.txt' >> /opt/wef/extra/delete-creds.sh
 	mv templates /opt/wef/main/ 2>/dev/null
-	sleep 0.6
-	echo -ne "\n[+] Downloading necesary files."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2
+	sleep 0.7
+	echo -ne "\n[+] Downloading necesary files"; sleep 0.3; echo -ne "."; sleep 0.3; echo -ne "."; sleep 0.3; echo -ne "."; sleep 0.3
 	wget "https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz" &>/dev/null
 	mv rockyou.txt.gz /opt/wef/main/wordlists/rockyou.txt.gz 2>/dev/null
 	gunzip /opt/wef/main/wordlists/rockyou.txt.gz 2>/dev/null
@@ -23,7 +28,7 @@ sleep 0.1
 	mv probable-v2-wpa-top4800.txt /opt/wef/main/wordlists/ 2>/dev/null
 	wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top10000.txt" &>/dev/null
 	mv darkweb2017-top10000.txt /opt/wef/main/wordlists/ 2>/dev/null
-	echo -ne "\n[+] Giving permissions to different files."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2
+	echo -ne "\n[+] Giving permissions to different files"; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2
 	sleep 0.4
 	cp ${adir}/WEF /usr/bin/wef 2>/dev/null
 	cp ${adir}/WEF /opt/wef/wef 2>/dev/null
@@ -33,7 +38,7 @@ sleep 0.1
 	chmod +x /usr/bin/wef 2>/dev/null
 	chmod +x /opt/wef/clear-logs.sh 2>/dev/null
 	rm ${adir}/setup.sh 2>/dev/null
-	echo -ne "\n[+] Installing requirements."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2; echo -ne "."; sleep 0.2
+	echo -ne "\n[+] Installing requirements"; sleep 0.3; echo -ne "."; sleep 0.3; echo -ne "."; sleep 0.3; echo -ne "."; sleep 0.3
 	pip3 install -r requirements.txt &>/dev/null
 	sleep 0.2
 	echo -e "\n[+] Installation completed, I hope you enjoy WEF\n"
