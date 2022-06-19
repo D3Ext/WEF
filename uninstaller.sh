95475bf965f60e5a278fddf15dba785f7a070731
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-06-19 21:56:28 +0200

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -5,9 +5,14 @@ blueColour="\e[0;34m\033[1m"
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
 
-echo -ne "\n${blueColour}[${endColour}${yellowColour}+${endColour}${blueColour}] Uninstalling WEF"; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne ".${endColour}"; sleep 0.5
-rm -rf /opt/wef 2>/dev/null
-rm /usr/bin/wef 2>/dev/null
-sleep 1
-echo -e "\n${blueColour}[${endColour}${greenColour}+${endColour}${blueColour}] WEF uninstalled successfully${endColour}"
-sleep 0.3
+if [ "$(id -u)" == "0" ]; then
+  echo -ne "\n${blueColour}[${endColour}${yellowColour}+${endColour}${blueColour}] Uninstalling WEF"; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne ".${endColour}"; sleep 0.5
+  rm -rf /opt/wef 2>/dev/null
+  rm /usr/bin/wef 2>/dev/null
+  sleep 1
+  echo -e "\n${blueColour}[${endColour}${greenColour}+${endColour}${blueColour}] WEF uninstalled successfully${endColour}"
+  sleep 0.2
+else
+  echo -e "\n${blueColour}[${endColour}${yellowColour}X${endColour}${blueColour}] Please, execute the script as root${endColour}"
+  exit 0
+fi
