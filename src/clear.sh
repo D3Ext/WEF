880fd4d1e3d4818e202c2a175fbb1b4d6b8dc66e
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-08-21 17:50:41 +0200

    Update clear.sh

--- src/clear.sh
+++ src/clear.sh
@@ -10,18 +10,20 @@ grayColour="\e[0;37m\033[1m"
 
 # Main Function
 if [ "$(id -u)" == "0" ]; then
-  for name in $(ls /opt/wef/main/captures); do
-    rm -rf /opt/wef/main/captures/$name 2>/dev/null
-    rm /opt/wef/main/logs/* 2>/dev/null
-  done
+  # Remove captures (.pcap) and logs (custom files of WEF to store attacks info)
+  rm -rf /opt/wef/main/captures/* 2>/dev/null
+  rm -rf /opt/wef/main/logs/* 2>/dev/null
 
   sleep 0.4
   echo -e "\n${blueColour}[${endColour}${greenColour}+${endColour}${blueColour}] Information and logs cleared\n${endColour}"
   sleep 0.3
+  # Simple function to remove the submitted credentials of the Evil AP if the user 
   echo -ne "\n${blueColour}[${endColour}${greenColour}+${endColour}${blueColour}] Do you want to clear credentials from the EvilTwin attack? [y/n]: ${endColour}" && read option
   if [ "$option" == "y" ] || [ "$option" == "yes" ]; then
     rm /opt/wef/main/templates/*/datos-privados.txt 2>/dev/null
     rm /opt/wef/main/templates/*/usernames.txt 2>/dev/null
+  else
+    echo -e "\n${blueColour}[${endColour}${greenColour}+${endColour}${blueColour}] Bye!\n${endColour}"
   fi
   
 else
