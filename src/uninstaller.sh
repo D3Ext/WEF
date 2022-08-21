6f8eb806054b5f0cd2dbe80c1016f3f95c74204d
D3Ext <67125397+D3Ext@users.noreply.github.com>
2022-08-21 17:53:07 +0200

    Update uninstaller.sh

--- src/uninstaller.sh
+++ src/uninstaller.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Colors
 yellowColour="\e[0;33m\033[1m"
 blueColour="\e[0;34m\033[1m"
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
 
+# Check uid
 if [ "$(id -u)" == "0" ]; then
   echo -ne "\n${blueColour}[${endColour}${yellowColour}+${endColour}${blueColour}] Uninstalling WEF"; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne "."; sleep 0.5; echo -ne ".${endColour}"; sleep 0.5
+  # Remove program from path and the wef folder structure under /opt/wef/
   rm -rf /opt/wef 2>/dev/null
   rm /usr/bin/wef 2>/dev/null
   sleep 1
